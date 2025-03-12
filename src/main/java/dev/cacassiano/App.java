47352aae39ef0eed78abbaab20a11e17458c134a
Cassiano <saurinho.64.ss@gmail.com>
2025-03-11 22:13:49 -0300

    first commit

---
+++ src/main/java/dev/cacassiano/App.java
@@ -0,0 +1,13 @@
+package dev.cacassiano;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
