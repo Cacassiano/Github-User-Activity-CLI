09fc4020efb537e3a55c01ab47fa1ea9429b4b8a
Cacassiano <saurinho.64.ss@gmail.com>
2025-03-14 12:07:25 -0300

    Update Issue #3 App.java

--- src/main/java/dev/cacassiano/App.java
+++ src/main/java/dev/cacassiano/App.java
@@ -2,15 +2,26 @@ package dev.cacassiano;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-
+import java.util.Scanner;
 public class App 
 {
     public static void main( String[] args ) throws IOException,URISyntaxException, InterruptedException
     {
-        String uri = "https://api.github.com/users/cacassiano/events";
+        
+        String uri = uri();
         Json meuJson = new Json();
         GitHubEvent[] events = meuJson.criarObjetoEventos(uri);
         
         System.out.println(events[0].id);
+        
     }   
+
+    public static String uri()
+    {
+        Scanner scan = new Scanner();
+        System.out.printf("Digite o nome da pessoa que quer buscar: ");
+        String user = scan.nextLine(); 
+
+        return "https://api.github.com/users/"+user+"/events";
+    }
 }
