1ec6190f0ec1e6970bc9ac57d9da500ccdfe79ac
Cassiano <saurinho.64.ss@gmail.com>
2025-03-14 21:23:13 -0300

    Programa finalizado

--- src/main/java/dev/cacassiano/App.java
+++ src/main/java/dev/cacassiano/App.java
@@ -13,9 +13,48 @@ public class App
     {
         String uri = uri();
         Json meuJson = new Json();
- 
         GitHubEvent[] events = meuJson.criarObjetoEventos(uri);
-        System.out.println(events[0].id);
+        if (events.length >=1) {
+            
+        
+            int comentIssue = 0;
+            int issueCriado = 0;
+            int repoCriado = 0;
+            int pushs = 0;
+            for (GitHubEvent e : events) {
+                if(e.type.equals("IssuesEvent"))
+                {
+                    if(e.payload.action.equals("opened"))
+                    {
+                        issueCriado++;
+                    }
+                }
+
+                if(e.type.equals("PushEvent"))
+                {
+                    pushs++;
+                }
+
+                if(e.type.equals("IssueCommentEvent"))
+                {
+                    comentIssue++;
+                }
+
+                if(e.type.equals("CreateEvent"))
+                {
+                    if(e.payload.ref_type.equals("repository"))
+                    {
+                        repoCriado++;
+                    }
+                    
+                    
+                }
+            }
+            System.out.println("O usuario "+events[0].actor.login+":\n\tCriou "+repoCriado+" repositorios\n\tCriou "+issueCriado+" Issues\n\tComentou em "+comentIssue+" Issues\n\tE fez "+pushs+" Pushes este ano");
+    
+        } else{
+            System.out.println("Não encontrei nenhuma atividade!");
+        }
     }   
  
     public static String uri() throws IOException,URISyntaxException, InterruptedException
